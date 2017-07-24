60da56cb204c7df6c21a8500507be4b52335cff3
DESKTOP-955G2G9\ccc <qma@cccis.com>
2017-07-24 15:29:27 +0800

    python basic

---
+++ 1_basic/basicType.py
@@ -0,0 +1,107 @@
+# Python中单行注释以 # 开头，实例如下：
+# 行与缩进
+# python最具特色的就是使用缩进来表示代码块，不需要使用大括号({})。
+# 缩进的空格数是可变的，但是同一个代码块的语句必须包含相同的缩进空格数。实例如下：
+
+# import 与 from...import
+# 在 python 用 import 或者 from...import 来导入相应的模块。
+# 将整个模块(somemodule)导入，格式为： import somemodule
+# 从某个模块中导入某个函数,格式为： from somemodule import somefunction
+# 从某个模块中导入多个函数,格式为： from somemodule import firstfunc, secondfunc, thirdfunc
+# 将某个模块中的全部函数导入，格式为： from somemodule import *
+
+import sys
+
+if True:
+	print (sys.argv)
+else:
+	print ("False")
+
+#python无需申明类型
+counter = 100  # 整型变量
+miles = 1000.0  # 浮点型变量
+name = "runoob"  # 字符串
+
+print(counter)
+print(miles)
+print(name)
+
+# Python3 中有六个标准的数据类型：
+# Number（数字）
+a = 309.19
+b = 35.22
+c = a*b
+print(c)
+
+# String（字符串）
+str = 'Runoob'
+print(str)  # 输出字符串
+print(str[0:-1])  # 输出第一个到倒数第二个的所有字符
+print(str[0])  # 输出字符串第一个字符
+print(str[2:5])  # 输出从第三个开始到第五个的字符
+print(str[2:])  # 输出从第三个开始的后的所有字符
+print(str * 2)  # 输出字符串两次
+print(str + "TEST")  # 连接字符串
+print('Ru\noob')
+
+# List（列表）使用[]
+#  列表可以完成大多数集合类的数据结构实现。列表中元素的类型可以不相同，它支持数字，字符串甚至可以包含列表（所谓嵌套）。
+list = ['abcd', 786, 2.23, 'runoob', 70.2]
+tinylist = [123, 'runoob']
+
+print(list)  # 输出完整列表
+print(list[0])  # 输出列表第一个元素
+print(list[1:3])  # 从第二个开始输出到第三个元素
+print(list[2:])  # 输出从第三个元素开始的所有元素
+print(tinylist * 2)  # 输出两次列表
+print(list + tinylist)  # 连接列表
+
+# Tuple（元组）使用()
+tuple = ('abcd', 786, 2.23, 'runoob', 70.2)
+# 元组（tuple）与列表类似，不同之处在于元组的元素不能修改。元组写在小括号(())里，元素之间用逗号隔开。
+tinytuple = (123, 'runoob')
+
+print(tuple)  # 输出完整元组
+print(tuple[0])  # 输出元组的第一个元素
+print(tuple[1:3])  # 输出从第二个元素开始到第三个元素
+print(tuple[2:])  # 输出从第三个元素开始的所有元素
+print(tinytuple * 2)  # 输出两次元组
+print(tuple + tinytuple)  # 连接元组
+
+# Sets（集合）使用{}
+# 集合（set）是一个无序不重复元素的序列。
+# 基本功能是进行成员关系测试和删除重复元素。
+# 可以使用大括号 { } 或者 set() 函数创建集合，注意：创建一个空集合必须用 set() 而不是 { }，因为 { } 是用来创建一个空字典。
+student = {'Tom', 'Jim', 'Mary', 'Tom', 'Jack', 'Rose'}
+print(student)  # 输出集合，重复的元素被自动去掉
+# 成员测试
+if ('Rose' in student):
+    print('Rose 在集合中')
+else:
+    print('Rose 不在集合中')
+# set可以进行集合运算
+a = set('abracadabra')
+b = set('alacazam')
+print(a)
+print(a - b)  # a和b的差集
+print(a | b)  # a和b的并集
+print(a & b)  # a和b的交集
+print(a ^ b)  # a和b中不同时存在的元素
+
+# Dictionary（字典）
+# 列表是有序的对象结合，字典是无序的对象集合。两者之间的区别在于：字典当中的元素是通过键来存取的，而不是通过偏移存取。
+# 字典是一种映射类型，字典用"{ }"标识，它是一个无序的键(key) : 值(value)对集合。
+# 键(key)必须使用不可变类型。
+# 在同一个字典中，键(key)必须是唯一的。
+dict = {}
+dict['one'] = "1 - 菜鸟教程"
+dict[2] = "2 - 菜鸟工具"
+
+tinydict = {'name': 'runoob', 'code': 1, 'site': 'www.runoob.com'}
+
+print(dict['one'])  # 输出键为 'one' 的值
+print(dict[2])  # 输出键为 2 的值
+print(tinydict)  # 输出完整的字典
+print(tinydict.keys())  # 输出所有键
+print(tinydict.values())  # 输出所有值
+
